52e3a003a1c645a3c272e4f1d8e66de90235fb8d
sagarkool200032 <sagarkool200032@gmail.com>
2020-02-06 22:36:53 +0530

    initial

---
+++ TicTacToe Game/README.md
@@ -0,0 +1,5 @@
+# Tic Tac Toe Game
+Using HTML,CSS,Javascript 
+#
+one click for X
+two clicks for O
---
+++ To Do App/ToDo/fApp/views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render
+from django.utils import timezone
+from fApp.models import Todo
+from django.http import HttpResponseRedirect
+
+# Create your views here.
+def index(request):
+    data = Todo.objects.all().order_by("-added_date")
+    dict = {"data":data}
+    return render(request,"fApp/index.html",dict)
+
+def add(request):
+    if request.method == 'POST':
+        currentDate = timezone.now()
+        content = request.POST["content"]
+        # to add data in database
+        created_obj = Todo.objects.create(added_date = currentDate, text = content)
+        print(currentDate)
+        print(content)
+        print(created_obj.id)   
+    return HttpResponseRedirect("/")
+
+def remove(request,todo_id):
+    # to delete data from database
+    Todo.objects.get(id=todo_id).delete()
+    return HttpResponseRedirect("/")
+
+    
